90c67a2abaff7a0445dfe6db010a309964e99595
yeison-mosquera <mosqueramosquerayeison9@gmail.com>
2025-04-22 15:56:18 -0500

    Se actualiza build

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+poetry run gunicorn django_crud_api_yei.wsgi:application
--- django_crud_api_yei/django_crud_api_yei/wsgi.py
+++ django_crud_api_yei/django_crud_api_yei/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_crud_api_yei.settings')
 
+
+
 application = get_wsgi_application()
